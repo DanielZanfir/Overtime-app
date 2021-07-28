c84c3a1d565d74d79988ecb7edc9266d10f49e12
DanielZanfir <danielzanfir98@gmail.com>
2021-07-28 18:10:11 +0300

    Added bootsrap, jquery and popper and added navbar to views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,20 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import "../channels"
+
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "jquery";
+import "popper.js";
+import "bootstrap"
+import "../stylesheets/aplication";
+
+
+
+
+
