d7165195df209759064d2c2556429bfa48f42652
DanielZanfir <danielzanfir98@gmail.com>
2021-07-29 13:27:28 +0300

    Added a method to show the full name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name:"Dani" , last_name:"Zanfir")
+  end
+
   describe "creation" do
-
-    before do
-      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name:"Dani" , last_name:"Zanfir")
-    end
-
     it "can be created" do
       expect(@user).to be_valid
     end
-
     it "can not be created without first name, last name" do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name methods" do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq("ZANFIR, DANI")
     end
   end
 end
