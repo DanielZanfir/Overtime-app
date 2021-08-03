794e55dbfb6a305e53ec21863c2af4fa2a865ebb
DanielZanfir <danielzanfir98@gmail.com>
2021-08-03 16:05:23 +0300

    removed status from new post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name:"Dani" , last_name:"Zanfir")
+AdminUser.create(email: "admin@user.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name:"Admin" , last_name:"User")
+
+puts "1 User and 1 AdminUser have been created"
+
 100.times do |post|
   Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
